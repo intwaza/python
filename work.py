7de4fd75b1bb654d396793de97bc69e1b18e09e3
intwaza <iintwazabelyse@gmail.com>
2021-03-30 00:23:24 +0300

    trial

---
+++ work.py
@@ -0,0 +1,23 @@
+name = input("what is your name? ")
+print("Hello " + name)
+
+
+birth_year = input("Enter your birth year ")
+age = 2021 - int(birth_year)
+print(age)
+
+
+Marks = 87
+if Marks > 90:
+    print("A")
+elif Marks > 80:
+    print("B")
+elif Marks > 70:
+    print("C")
+elif Marks > 60:
+    print("D")
+elif Marks > 50:
+    print("E")
+else:
+    print("F")
+
